2d71df24e79600027db277e1c461d06b62a382c3
selcuk.ari <selcukari51@gmail.com>
2025-01-30 17:13:58 +0300

    vuetify add

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Plugins
+import { registerPlugins } from './plugins'
+
 import './style.css'
 
 import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
-createApp(App)
+const app = createApp(App)
+
+registerPlugins(app)
+
+app
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
